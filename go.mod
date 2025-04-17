66e052e05861af0522e4649bbf4f93a379c31739
Matthias Scheidegger <mscheidegger@gmail.com>
2025-04-17 11:13:23 +0200

    Add mdgrpc package, starting with the gRPC resolver integration.

--- go.mod
+++ go.mod
@@ -28,5 +28,8 @@ require (
 	golang.org/x/sys v0.29.1-0.20250107080300-1c14dcadc3ab // indirect
 	golang.org/x/text v0.21.0 // indirect
 	golang.zx2c4.com/wireguard/windows v0.5.3 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	google.golang.org/grpc v1.71.1 // indirect
+	google.golang.org/protobuf v1.36.4 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/mdgrpc/resolver.go
@@ -0,0 +1,79 @@
+// Minidisc gRPC address resolver.
+//
+// This custom resolver allows using addresses of the form
+//     minidisc://name
+// or if you use labels
+//     minidisc://name?label1=value1&label2=value2
+//
+// To use, just call mdgrpc.RegisterResolver() before creating any gRPC client
+// connections.
+
+package grpcresolver
+
+import (
+	"github.com/mscheidegger/minidisc/pkg/minidisc"
+	"google.golang.org/grpc/resolver"
+)
+
+func RegisterResolver() {
+	resolver.Register(&minidiscResolverBuilder{})
+}
+
+type minidiscResolverBuilder struct {
+	resolver.Builder
+}
+
+func (mrb *minidiscResolverBuilder) Build(
+	tgt resolver.Target, cc resolver.ClientConn, _ resolver.BuildOptions,
+) (resolver.Resolver, error) {
+	name := tgt.URL.Host
+	labels := make(map[string]string)
+	q := tgt.URL.Query()
+	for key, _ := range q {
+		labels[key] = q.Get(key)
+	}
+	r := &minidiscResolver{
+		name:       name,
+		labels:     labels,
+		clientConn: cc,
+	}
+	go func() {
+		// Kick off first resolution at construction time. gRPC will apparently
+		// only do it after this initial attempt.
+		r.ResolveNow(resolver.ResolveNowOptions{})
+	}()
+	return r, nil
+}
+
+func (mrb *minidiscResolverBuilder) Scheme() string {
+	return "minidisc"
+}
+
+type minidiscResolver struct {
+	resolver.Resolver
+
+	name       string
+	labels     map[string]string
+	clientConn resolver.ClientConn
+}
+
+func (mr *minidiscResolver) ResolveNow(_ resolver.ResolveNowOptions) {
+	addr, err := minidisc.FindService(mr.name, mr.labels)
+	if err != nil {
+		mr.clientConn.ReportError(err)
+		return
+	}
+	mr.clientConn.UpdateState(resolver.State{
+		Endpoints: []resolver.Endpoint{
+			resolver.Endpoint{
+				Addresses: []resolver.Address{
+					resolver.Address{Addr: addr.String()},
+				},
+			},
+		},
+	})
+}
+
+func (mr *minidiscResolver) Close() {
+	// No-op
+}
